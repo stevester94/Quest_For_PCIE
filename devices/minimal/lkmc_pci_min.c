f797ac87dc64a132c556da112026c66c14e21272
Steven Mackey <stevester94@gmail.com>
2018-06-16 20:42:27 -0400

    Add minimal device/drivers, and socket testing demo

---
+++ devices/minimal/lkmc_pci_min.c
@@ -0,0 +1,73 @@
+#include "qemu/osdep.h"
+#include "hw/pci/pci.h"
+
+#define DEVICE_NAME "lkmc_pci_min"
+
+    typedef struct {
+        PCIDevice pdev;
+        MemoryRegion mmio;
+    } State;
+
+    static uint64_t mmio_read(void *opaque, hwaddr addr, unsigned size)
+    {
+        printf(DEVICE_NAME " mmio_read addr = %llx size = %x", (unsigned long long)addr, size);
+        return 0x1234567812345678;
+    }
+
+    static void mmio_write(void *opaque, hwaddr addr, uint64_t val,
+            unsigned size)
+    {
+        State *state = opaque;
+
+        printf(DEVICE_NAME " mmio_read addr = %llx val = %llx size = %x\n",
+                (unsigned long long)addr, (unsigned long long)val, size);
+        switch (addr) {
+            case 0x0:
+                pci_set_irq(&state->pdev, 1);
+                break;
+            case 0x4:
+                pci_set_irq(&state->pdev, 0);
+                break;
+        }
+    }
+
+    static const MemoryRegionOps mmio_ops = {
+        .read = mmio_read,
+        .write = mmio_write,
+        .endianness = DEVICE_NATIVE_ENDIAN,
+    };
+
+    static void realize(PCIDevice *pdev, Error **errp)
+    {
+        State *state = DO_UPCAST(State, pdev, pdev);
+
+        pci_config_set_interrupt_pin(pdev->config, 1);
+        memory_region_init_io(&state->mmio, OBJECT(state), &mmio_ops, state,
+                DEVICE_NAME, 8);
+        pci_register_bar(pdev, 0, PCI_BASE_ADDRESS_SPACE_MEMORY, &state->mmio);
+    }
+
+    static void class_init(ObjectClass *class, void *data)
+    {
+        PCIDeviceClass *k = PCI_DEVICE_CLASS(class);
+
+        k->realize = realize;
+        k->vendor_id = PCI_VENDOR_ID_QEMU;
+        k->device_id = 0x11e9;
+        k->revision = 0x0;
+        k->class_id = PCI_CLASS_OTHERS;
+    }
+
+    static const TypeInfo type_info = {
+        .name          = DEVICE_NAME,
+        .parent        = TYPE_PCI_DEVICE,
+        .instance_size = sizeof(State),
+        .class_init    = class_init,
+    };
+
+    static void register_types(void)
+    {
+        type_register_static(&type_info);
+    }
+
+    type_init(register_types)
---
+++ testing/socket_demo/simple_server.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include <pthread.h>
+#include <unistd.h>
+
+
+// Run this as thread
+void simple_tcp_server()
+{
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[2000];
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+     
+    //Receive a message from client
+    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
+    {
+        //Send the message back to client
+        write(client_sock , client_message , strlen(client_message));
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+}
+
+int main(int argc , char *argv[])
+{
+    /* this variable is our reference to the second thread */
+    pthread_t thread;
+
+    /* create a second thread which executes inc_x(&x) */
+    if(pthread_create(&thread, NULL, simple_tcp_server, NULL)) {
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+    }
+
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
